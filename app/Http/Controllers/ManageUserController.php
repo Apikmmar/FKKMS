6a72c9f0106745c9cbbbec868cc6c56994a2a45d
apikammar <apikammar07@gmail.com>
2023-12-17 00:26:48 +0800

    by ammar

--- app/Http/Controllers/ManageUserController.php
+++ app/Http/Controllers/ManageUserController.php
@@ -2,20 +2,33 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class ManageUserController extends Controller
 {
+    // redirect to register user
+    public function create() {
+        return view('ManageUser.Register');
+    }
+    public function createNewUser(Request $request) {
+        $input = $request->all();
+        User::create($input);
+
+        return redirect('ManageUser.Login');
+    }
+
     // Show the login form
     public function showLoginForm()
     {
-        return view('auth.login');
+        return view('ManageUser.Login');
     }
 
     // Handle login form submission
     public function login(Request $request)
     {
-        $credentials = $request->only('username', 'password', 'user_type');
+        $credentials = $request->only('username', 'password', 'user_role');
 
         if (Auth::attempt($credentials)) {
             // Authentication passed
@@ -23,7 +36,7 @@ class ManageUserController extends Controller
         }
 
         // Authentication failed
-        return back()->withInput($request->only('username', 'user_type'))
+        return back()->withInput($request->only('username', 'user_role'))
             ->withErrors(['Invalid credentials. Please try again.']);
     }
 }
--- resources/views/ManageUser/Register.blade.php
+++ resources/views/ManageUser/Register.blade.php
@@ -1,48 +1,49 @@
 @extends('component.Master')
 
 @section('content')
-<div align = "center" class = "container">
-            <form align = "center" action = "" method ="post" id = "login">
-				<br><br>
-				<center><img src="{{asset('images/fk.png')}}" width ="170" height="130"><center>
-                <div class ="header">
-                    <h3 style="font-size: 35px;color:  #000000 ;font-family: Playfair Display;"><center>Register Your Account</center></h3>
-                </div>
-	
-                <div class="box"></div>
-                <div class="input">
-                    <table style = "margin-left: auto;margin-right: auto">
+    <div align = "center" class = "container">
+        <form align="center" action="/register" method="post" id="login">
+            @csrf
+            <br><br>
+            <center><img src="{{asset('images/fk.png')}}" width ="170" height="130"><center>
+            <div class ="header">
+                <h3 style="font-size: 35px;color:  #000000 ;font-family: Playfair Display;"><center>Register Your Account</center></h3>
+            </div>
 
-                        <!-- Insert Username -->
-                        <tr>
-                            <td><input type = "text" name = "username" placeholder = "Username" required="required"></td>
-                        </tr>
-                        <!-- Insert Full Name -->
-                        <tr>
-                            <td><input type = "text" name = "name" placeholder = "Full Name" required="required"></td>
-                        </tr>
-                        <!-- Insert Email -->
-                        <tr>
-                            <td><input type = "text" name = "email" placeholder = "Email" required="required"></td>
-                        </tr>
-                        <!-- Insert Password -->
-                        <tr>
-                            <td><input type = "password" name = "password" placeholder = "Password" required="required"></td>
-                        </tr>
-                        <!-- Insert User type -->
-                        <tr>
-                            <td>User Type <select name = "user_type"><option>Admin</option><option>Kiosk Participant</option><option>FK Technical Team</option><option>Pupuk Admin</option><option>FK Bursary</option></select></td>
-                        </tr>
-                        <tr>
-                            <!-- <td><br><center><input type = "submit" name = "login" value = "Login" ></center></td> -->
-                            <td><br><center><input type="submit"  name="register" value = "Register" onclick = <?php $_SESSION['form'] = 'register'; ?>></center></td>
-                        </tr>
-                        <tr>
-                            <td><br><center><p>Already Registered?? <a href="Login.php">Click Here</a></p></td>
-                        </tr>
+            <div class="box"></div>
+            <div class="input">
+                <table style = "margin-left: auto;margin-right: auto">
 
-                    </table>
-			    </div>
-		</div>
-		</form>
-@endsection('content')
+                    <!-- Insert Username -->
+                    <tr>
+                        <td><input type = "text" name = "username" placeholder = "Username" required="required"></td>
+                    </tr>
+                    <!-- Insert Full Name -->
+                    <tr>
+                        <td><input type = "text" name = "full_name" placeholder = "Full Name" required="required"></td>
+                    </tr>
+                    <!-- Insert Email -->
+                    <tr>
+                        <td><input type = "text" name = "email" placeholder = "Email" required="required"></td>
+                    </tr>
+                    <!-- Insert Password -->
+                    <tr>
+                        <td><input type = "password" name = "password" placeholder = "Password" required="required"></td>
+                    </tr>
+                    <!-- Insert User type -->
+                    <tr>
+                        <td>User Type <select name = "user_role"><option>Admin</option><option>Kiosk Participant</option><option>FK Technical Team</option><option>Pupuk Admin</option><option>FK Bursary</option></select></td>
+                    </tr>
+                    <tr>
+                        <!-- <td><br><center><input type = "submit" name = "login" value = "Login" ></center></td> -->
+                        <td><br><center><input type="submit"  name="register" value = "Register" onclick = <?php $_SESSION['form'] = 'register'; ?>></center></td>
+                    </tr>
+                    <tr>
+                        <td><br><center><p>Already Registered?? <a href="/login">Click Here</a></p></td>
+                    </tr>
+
+                </table>
+            </div>
+        </form>
+    </div>
+@endsection
